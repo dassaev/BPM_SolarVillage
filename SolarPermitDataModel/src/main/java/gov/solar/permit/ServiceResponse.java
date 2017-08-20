393428b7539b617eff72a2c997d87125e2af9c31
bpmsAdmin <oyimaier@Yimaier-Laptop.(none)>
2017-08-20 18:48:17 -0500

    Added JAXB annotations

--- SolarPermitDataModel/src/main/java/gov/solar/permit/ServiceResponse.java
+++ SolarPermitDataModel/src/main/java/gov/solar/permit/ServiceResponse.java
@@ -1,11 +1,27 @@
 package gov.solar.permit;
 
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlType;
+
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlType(name = "", propOrder = {
+	    "applicationId",
+	    "structuralPermitStatus",
+	    "electricPermitStatus"
+	})
+@XmlRootElement(name="ServiceResponse")
 public class ServiceResponse implements java.io.Serializable {
 
 	private static final long serialVersionUID = 1L;
 	
+	@XmlElement(required = true)
 	private String applicationId;
+	@XmlElement(required = true)
 	private String structuralPermitStatus;
+	@XmlElement(required = true)
 	private String electricPermitStatus;
 	
 	
