6c42e2527e07c0620b8c4a5b6df2423fb853365e
bpmsAdmin <oyimaier@Yimaier-Laptop.(none)>
2017-08-20 19:13:22 -0500

    Changed JAXB annotations

--- SolarPermitDataModel/src/main/java/gov/solar/permit/ServiceResponse.java
+++ SolarPermitDataModel/src/main/java/gov/solar/permit/ServiceResponse.java
@@ -7,7 +7,7 @@ import javax.xml.bind.annotation.XmlRootElement;
 import javax.xml.bind.annotation.XmlType;
 
 @XmlAccessorType(XmlAccessType.FIELD)
-@XmlType(name = "", propOrder = {
+@XmlType(name = "ServiceResponse", propOrder = {
 	    "applicationId",
 	    "structuralPermitStatus",
 	    "electricPermitStatus"
